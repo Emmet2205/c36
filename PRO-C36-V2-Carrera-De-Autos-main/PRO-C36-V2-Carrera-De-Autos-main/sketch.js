df758c271d6ab87b3d463c4cea519a414bf0dcfc
Emmet2205 <92766075+Emmet2205@users.noreply.github.com>
2022-04-22 11:57:36 -0500

    Add files via upload

---
+++ PRO-C36-V2-Carrera-De-Autos-main/PRO-C36-V2-Carrera-De-Autos-main/sketch.js
@@ -0,0 +1,30 @@
+
+//Código Alumno PRO C36 V2 Estructurar antes de Programar;
+
+var canvas;
+var backgroundImage;
+var bgImg;
+var database;
+var form, player;
+var playerCount;
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.start();
+  bgImg = backgroundImage
+
+}
+
+function draw() {
+  background(bgImg);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
